99774ccbbb3894dd7245de26537ba7e36956245b
Naoise Golden Santos <naoise.golden@gmail.com>
2023-12-21 00:23:14 +0100

    Comment out Auth0 code

--- src/routes/requests.routes.ts
+++ src/routes/requests.routes.ts
@@ -5,12 +5,12 @@ import {
     uploadImageWithBase64,
     uploadRequest
 } from "../controllers/requests.controller";
-import {checkJwtMiddleware} from "../middlewares/checkJwt.middleware";
+// import {checkJwtMiddleware} from "../middlewares/checkJwt.middleware";
 
 export const requestRouter = Router()
 
 requestRouter.get("/public", publicRequest)
-requestRouter.get("/protected", checkJwtMiddleware, protectedRequest)
+// requestRouter.get("/protected", checkJwtMiddleware, protectedRequest)
 requestRouter.post("/upload", uploadRequest)
 requestRouter.post("/base", uploadImageWithBase64)
 
